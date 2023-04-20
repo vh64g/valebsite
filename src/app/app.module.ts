bde00f431e7479ae5cf2b79cabf522090a131439
Valentin <valentin.heiderich@gmx.de>
2023-04-20 22:19:58 +0200

    fc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'valliprivateonweb',
+    appId: '1:577249226633:web:eff365f8e9cb1d3d3abb58',
+    storageBucket: 'valliprivateonweb.appspot.com',
+    apiKey: 'AIzaSyAiVWBDYa0CsiFlbb4PnEaSp0ntiUIVWNU',
+    authDomain: 'valliprivateonweb.firebaseapp.com',
+    messagingSenderId: '577249226633',
+    measurementId: 'G-M76NZ530K1',
+  },}
